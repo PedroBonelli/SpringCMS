f88e442411d8bcac9192387a8f8f4b6a8a5bdace
PedroBonelli <128149892+PedroBonelli@users.noreply.github.com>
2024-11-03 23:04:12 -0300

    Delete SpringCMS/SpringCMS directory

--- SpringCMS/SpringCMS/src/main/java/com/github/pedrobonelli33/SpringCMS/repository/ContentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.pedrobonelli33.SpringCMS.repository;
-
-import com.github.pedrobonelli33.SpringCMS.model.Content;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContentRepository extends JpaRepository<Content, Long> {
-}
